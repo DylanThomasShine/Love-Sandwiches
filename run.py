69cfbfc294cd8d1d0b7d6e8ce3122ac33d433aed
Dylan <dylanthomasshine@gmail.com>
2021-06-22 10:14:11 +0000

    Refactor update functions into one

--- run.py
+++ run.py
@@ -71,7 +71,17 @@ def update_surplus_worksheet(data):
     print("Updating surplus worksheet.....\n")
     surplus_worksheet = SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
-    print("Surplus workspace added successfully.\n")
+    print("Surplus worksheet added successfully.\n")
+
+def update_worksheet(data, worksheet):
+    """
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet.....\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
 
 
 def calculate_surplus_data(sales_row):
@@ -100,9 +110,9 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
     print(new_surplus_data)
      
 
